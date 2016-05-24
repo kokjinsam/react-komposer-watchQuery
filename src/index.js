29fa24b2267c85b99b4542a73232c9d8365eb1a1
skaxwng <samkj.ks@gmail.com>
2016-05-24 12:38:55 -0500

    Added checks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,40 @@ import { compose } from 'react-komposer';
 import gql from 'apollo-client/gql';
 
 export function composeWatchQueryBase(options, fn, props, onData) {
-  const { Client } = props.context();
+  if (options === null || typeof options !== 'object') {
+    const error = 'No options object passed as argument';
+    throw new Error(error);
+  }
+
+  if (!options.hasOwnProperty('query')) {
+    const error = 'query is missing';
+    throw new Error(error);
+  }
+
+  if (typeof options.query !== 'string') {
+    const error = 'query should be string';
+    throw new Error(error);
+  }
+
+  if (fn === null || typeof fn !== 'function') {
+    const error = 'No resultMapper function passed as argument';
+    throw new Error(error);
+  }
+
+  if (!props.context) {
+    const error = 'No context passed as prop';
+    throw new Error(error);
+  }
+
+  const context = typeof props.context === 'function' ? props.context() : props.context;
+
+  const Client = context.Client || context.client;
+
+  if (!Client) {
+    const error = 'No Apollo Client found in the context';
+    throw new Error(error);
+  }
+
   const {
     query,
     pollInterval = 500,
